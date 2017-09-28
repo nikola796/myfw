ca26fe3a59d06555a76b181f5d1bfff7f7b9fc60
nikola796 <nikola796@gmail.com>
2017-09-28 09:09:47 +0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+
+return array(
+
+'database' => array(
+'host' => 'mysql:host=localhost',
+'db_name' => '',
+'user' => '',
+'password' => '',
+'option' => array(
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
+
+	)
+)
+	);
